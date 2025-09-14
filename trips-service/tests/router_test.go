bdbf7e9a522a20843c9f348af45529fec62bd449
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2025-09-14 18:19:09 +0200

    test(trips-service): add health route test

---
+++ trips-service/tests/router_test.go
@@ -0,0 +1,33 @@
+package tests
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"trips-service.com/src/config"
+	"trips-service.com/src/router"
+)
+
+func TestHealthRoute(t *testing.T) {
+	env := &config.Env{}
+	r := router.Init(env)
+	req := httptest.NewRequest(http.MethodGet, "/api/trips/health", nil)
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Fatalf("expected 200 ok, got: %d", w.Code)
+	}
+
+	var res map[string]string
+	if err := json.Unmarshal(w.Body.Bytes(), &res); err != nil {
+		t.Fatal("Invalid json responsse")
+	}
+
+	if res["status"] != "ok" {
+		t.Fatalf("expected status to contain string 'ok', got: %s", res["status"])
+	}
+}
