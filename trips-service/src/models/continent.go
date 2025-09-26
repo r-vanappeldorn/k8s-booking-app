136a92a495adee3313a0148c201dab0d0811fda8
Rajeev van Appeldorn <rvanappeldorn@gmail.com>
2025-09-26 19:28:08 +0200

    fix(trips-service): add missing types

--- trips-service/src/models/continent.go
+++ trips-service/src/models/continent.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Continent struct {
-	ID        uint `gorm:"primarykey;autoIncrement"`
-	code      string
-	name      string
+	ID        uint           `gorm:"primarykey;autoIncrement"`
+	code      string         `gorm:"type: char(2). not null"`
+	name      string         `gorm:"type: varchar(200). not null"`
 	CreatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP"`
 	UpdatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"`
 	DeletedAt gorm.DeletedAt `gorm:"index"`
